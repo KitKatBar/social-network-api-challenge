9489cf3f064786c2639c7b60a229ab3af681334c
Katriel Chiu <katriel_chiu@msn.com>
2024-05-18 03:06:56 -0700

    implemented thought routes and tested it all in Insomnia and it works

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
 
-router.route('/').get().post();
+const {
+    getThoughts,
+    getThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
 
-router.route('/:thoughtId').get().put().delete();
+router.route('/').get(getThoughts).post(createThought);
 
-router.route('/:thoughtId/reactions').post();
+router.route('/:thoughtId').get(getThought).put(updateThought).delete(deleteThought);
 
-router.route('/:thoughtId/reactions/:reactionId').delete();
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
